f478eba9e9cac5f9f6681d2a7d9839a0114e6276
TedLeem <wjdxor6346@naver.com>
2021-01-16 10:30:05 +0900

    두번쨰

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import PropTypes from "prop-types";
+const movieList = [
+  {name:"iron man", year: 2010},
+  {name:"iron man2", year: 2012},
+  {name:"iron man3", year: 2014}
+  ]
+
+
+function Movie(params) {
+  return (
+    <li>  
+      <h2>
+        Movie Name:  {params.name}    
+      </h2>
+      <h3> Movie year: {params.year} </h3>
+    </li>
+  )
+}
+
+Movie.prototype= {
+  name: PropTypes.string.isRequired,
+  year: PropTypes.number
+}
 
 function App() {
   return (
     <div>
       <h1>
-        hello
+        hello this is a Movie list
       </h1>
+      <ul>  
+        {movieList.map(params => <Movie 
+        id = {params.key}
+        name={params.name} 
+        year={params.year} 
+        
+        />)}
+      </ul>
     </div>
   );
 }
